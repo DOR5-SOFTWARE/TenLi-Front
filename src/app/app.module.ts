de19d2a636ab61ffeff326b67dc3797dc610cde3
Anton Dorofeyev <dorofeyev@gmail.com>
2017-04-11 10:21:58 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ClipboardModule } from 'ngx-clipboard';
 
-import { RandomUsersGeneratorComponent } from './random-users-generator/random-users-generator.component';
+import { RandomUsersGeneratorComponent } from './components/random-users-generator/random-users-generator.component';
+import { ModalMessageComponent } from './components/modal-message/modal-message.component';
+
 import { RandomUsersService } from './services/random-users.service';
 
 @NgModule({
   declarations: [
-    RandomUsersGeneratorComponent
+    RandomUsersGeneratorComponent,
+    ModalMessageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ClipboardModule
   ],
   providers: [
     RandomUsersService
--- src/app/models/random-user/random-user.model.ts
+++ src/app/models/random-user/random-user.model.ts
@@ -1,14 +1,14 @@
 import { Address } from './properties/address.model';
 import { Name } from './properties/name.model'
 import { Image } from './properties/image.model'
-import { Gender } from './properties/gender.model' 
+import { Gender } from './properties/gender.model'
 
 export class RandomUser {
-  firstname : Name;
-  lastname : Name;
-  image : Image;
-  gender : Gender;
-  profession : string;
-  address : Address;
-  phoneNumber : string;
+  firstname: Name;
+  lastname: Name;
+  image: Image;
+  gender: Gender;
+  email: string;
+  address: Address;
+  phoneNumber: string;
 }
